8b1c9b1d42521cd7acb5421eb2ac99c11cb47fb2
kamel besseghier <bbs42000@hotmail.fr>
2023-06-06 13:48:39 +0200

    model

---
+++ db/migrate/20230606114440_create_claims.rb
@@ -0,0 +1,11 @@
+class CreateClaims < ActiveRecord::Migration[7.0]
+  def change
+    create_table :claims do |t|
+      t.string :title
+      t.text :content
+      t.references :realty, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,59 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_111849) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_114604) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "claims", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "realty_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["realty_id"], name: "index_claims_on_realty_id"
+  end
+
+  create_table "documents", force: :cascade do |t|
+    t.string "name"
+    t.bigint "realty_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["realty_id"], name: "index_documents_on_realty_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "realty_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["realty_id"], name: "index_messages_on_realty_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "realties", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "type"
+    t.string "rent"
+    t.date "payment_date"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_realties_on_user_id"
+  end
+
+  create_table "tenants", force: :cascade do |t|
+    t.date "enter_date"
+    t.date "out_date"
+    t.bigint "realty_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["realty_id"], name: "index_tenants_on_realty_id"
+    t.index ["user_id"], name: "index_tenants_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +80,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_111849) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "claims", "realties"
+  add_foreign_key "documents", "realties"
+  add_foreign_key "messages", "realties"
+  add_foreign_key "messages", "users"
+  add_foreign_key "realties", "users"
+  add_foreign_key "tenants", "realties"
+  add_foreign_key "tenants", "users"
 end
