0adf797d5bc672a728f0e94bdaa33c37817379e6
Medjou Mehdi <medjou.mehdi@hotmail.com>
2023-06-06 13:49:43 +0200

    no

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_111849) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_113505) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "realties", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "type"
+    t.string "rent"
+    t.date "payment_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
