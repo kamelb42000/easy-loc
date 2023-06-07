5ec1ed87f1db764cd1701fbf4c66f66640050cd9
Billal Medjou <medjoubillal@gmail.com>
2023-06-07 15:07:22 +0200

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
   resources :realties do
-
-    resources :claims, only: [:index,:new,:create]
+    resources :claims, only: [:index, :new, :create, :edit, :update]
     resources :documents, only: [:index, :create]
-
     resources :messages, only: [:index, :create]
-    resources :tenants, only:[:create, :new]
+    resources :tenants, only: [:create, :new]
   end
 
-  resources :claims, only: [:show, :edit, :update, :destroy]
-
-
+  resources :claims, only: [:show, :destroy]
 end
