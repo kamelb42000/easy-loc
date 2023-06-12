54c263b1cedc2b8b05b544c9236d8edb9d4168f7
Medjou Mehdi <medjou.mehdi@hotmail.com>
2023-06-12 10:05:00 +0200

    messagerie

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,14 +17,9 @@ puts "Creating users ..."
 realty1 = Realty.create!(name: "Loft", address: "20 rue des capucins", user: user1, category: "loft", rent: "20€")
 puts "Creating realties ..."
 
-
 Message.create!(content: "Salut, çava ?", user: user1, realty: realty1)
 
-Document1 = Document.create(name:"1er document", realty: realty1)
-Document2 = Document.create(name:" document", realty: realty1)
-Document3 = Document.create(name:" document", realty: realty1)
-Document4 = Document.create(name:" document", realty: realty1)
-Document5 = Document.create(name:" document", realty: realty1)
+
 puts "Document created!"
 
 Claim.create!(title: "Chaudière", content: "Super", realty_id: realty1.id)
