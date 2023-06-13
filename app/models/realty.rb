593d4af1e13f25283bbf3776c38aeb7fb92d47a3
Medjou Mehdi <medjou.mehdi@hotmail.com>
2023-06-13 12:52:00 +0200

    seed update

--- app/models/realty.rb
+++ app/models/realty.rb
@@ -1,8 +1,8 @@
 class Realty < ApplicationRecord
   has_many :messages, dependent: :destroy
-  has_many :claims, dependent: :destroy
   has_many :documents, dependent: :destroy
   has_many :tenants, dependent: :destroy
+  has_many :claims, dependent: :destroy
   belongs_to :user
 
   attr_accessor :tenant_user_id, :tenant_enter_date, :tenant_out_date
